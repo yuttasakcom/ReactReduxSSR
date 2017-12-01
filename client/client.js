73a9f2e9981df7372c0796a86a8848658ddf854a
YoProgrammer <yuttasakcom@gmail.com>
2017-12-01 23:47:50 +0700

    create store in client

--- client/client.js
+++ client/client.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import reducers from '@/reducers'
+
+import { BrowserRouter } from 'react-router-dom'
 import Routes from '@/routes'
 
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 , document.getElementById('root'))
