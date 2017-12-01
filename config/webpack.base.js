25047e4c7fe0804de63ad1792396430f18386407
YoProgrammer <yuttasakcom@gmail.com>
2017-12-01 16:34:05 +0700

    create webpack-base.js

---
+++ config/webpack.base.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.resolve(__dirname, '..', dir)
+}
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': resolve('client')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            'react',
+            'stage-2',
+            'env'
+          ]
+        }
+      }
+    ]
+  }
+}
